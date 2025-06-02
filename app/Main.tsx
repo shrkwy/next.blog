5ae456c6073142ece44668eb18ff3d535857386d
shrkwt <ss.sharma.shantnu@gmail.com>
2025-06-02 09:22:22 +0530

    update (home layout, image lazy loading, thumbnail on homepage)

--- app/Main.tsx
+++ app/Main.tsx
@@ -3,21 +3,154 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 import NewsletterForm from 'pliny/ui/NewsletterForm'
+import Image from '@/components/Image'
 
-const MAX_DISPLAY = 5
+const MAX_DISPLAY = 6
 
 export default function Home({ posts }) {
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
-            Latest
+        {/* <div className="space-y-2 pt-6 pb-6 md:space-y-5">
+          <span className="relative inset-y-4 ml-24 inline-block px-1 before:absolute before:-inset-1 before:block before:-skew-y-6 before:rounded-lg before:bg-primary-600 before:bg-opacity-20 sm:inset-y-7 sm:ml-40">
+            <span className="relative inline-block -rotate-6 text-primary-500">
+              <Link
+                href="https://youtube.com/@shrkwt"
+                className="font-arrow2 text-sm font-bold text-primary-500 transition hover:underline hover:underline-offset-8 sm:text-xl"
+              >
+                @shrkwt
+              </Link>
+            </span>
+          </span>
+          <h1 className="text-background-color pt-2 text-4xl font-bold leading-9 tracking-tight dark:text-gray-100 sm:text-5xl sm:leading-10 md:text-6xl md:leading-snug">
+            <span className="animate-wavingHand">👋🏻</span>, I am
+            <span className="font-arrow relative inset-y-8 ml-2 inline-block -rotate-12 text-primary-500 sm:inset-y-14">
+              ^
+            </span>
+            Shantnu.
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+          <h2 className="prose pt-5 text-lg text-gray-600 dark:text-gray-300">
             {siteMetadata.description}
-          </p>
+          </h2>
+          <div className="leading-7 text-gray-500 underline underline-offset-4 sm:pr-6 sm:text-lg">
+            <Link
+              href="/about"
+              className="hover:cursor-pointer hover:text-primary-500 dark:text-gray-500 hover:dark:text-primary-500"
+            >
+              <p>Read the rest of my bio &rarr;</p>
+            </Link>
+          </div>
+          <h3 className="my-4 mt-16 pt-20 pb-2 text-3xl font-bold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl">
+            Recent Posts{' '}
+          </h3>
+      </div>*/}
+        <div className="space-y-6 md:my-16">
+          <div className="flex flex-col-reverse gap-8 md:flex-row md:justify-between">
+            <div className="space-y-4 md:max-w-lg">
+              <span className="relative inset-y-4 ml-24 inline-block px-1 before:absolute before:-inset-1 before:block before:-skew-y-6 before:rounded-lg before:bg-primary-600 before:bg-opacity-20 sm:inset-y-7 sm:ml-40">
+                <span className="relative inline-block -rotate-6 text-primary-500">
+                  <Link
+                    href="https://youtube.com/@shrkwt"
+                    className="font-arrow2 text-sm font-bold text-primary-500 transition hover:underline hover:underline-offset-8 sm:text-xl"
+                  >
+                    @shrkwt
+                  </Link>
+                </span>
+              </span>
+              <h1 className="text-background-color pt-2 text-4xl font-bold leading-9 tracking-tight dark:text-gray-100 sm:text-5xl sm:leading-10 md:text-6xl md:leading-snug">
+                <span className="animate-wavingHand">👋🏻</span>, I am
+                <span className="font-arrow relative inset-y-8 ml-2 inline-block -rotate-12 text-primary-500 sm:inset-y-14">
+                  ^
+                </span>
+                <span className="absolute">
+                  Shantnu<span className="text-orange-400 ">.</span>
+                </span>
+              </h1>
+              <h2 className="prose pt-5 text-lg text-gray-600 dark:text-gray-300">
+                {siteMetadata.description}
+              </h2>
+              <div className="leading-7 text-gray-500 underline underline-offset-4 sm:pr-6 sm:text-lg">
+                <Link
+                  href="/about"
+                  className="hover:cursor-pointer hover:text-primary-500 dark:text-gray-500 hover:dark:text-primary-500"
+                >
+                  <p>Read the rest of my bio &rarr;</p>
+                </Link>
+              </div>
+            </div>
+            <div className="pt-10">
+              <div className="relative h-20 w-20 md:h-28 md:w-28">
+                <Image
+                  src="/static/images/avatar.jpeg"
+                  className="rounded-full"
+                  width={112}
+                  height={112}
+                  alt="Avatar image"
+                  loading="eager"
+                  priority
+                />
+                <div className="absolute inset-0 -z-10 bg-gradient-to-tl from-purple-700 to-orange-700 opacity-0 blur-2xl md:opacity-70" />
+              </div>
+            </div>
+          </div>
         </div>
+        <div>
+          <div className="grid grid-cols-1 gap-8 pt-10 pb-10 md:grid-cols-2 xl:grid-cols-3">
+            {!posts.length && 'No posts found.'}
+            {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
+              const { slug, date, title, summary, tags, thumbnail } = frontMatter
+              const tagsToShow = tags.slice(0, 1)
+              return (
+                <article
+                  key={slug}
+                  className="group relative flex h-full transform rounded-lg bg-transparent bg-opacity-20 p-2 transition duration-100 hover:scale-105 hover:rounded-xl hover:bg-gray-100 dark:hover:bg-gray-800"
+                >
+                  <div>
+                    <div className="group relative max-h-4 overflow-hidden rounded-lg pb-60">
+                      <Link href={`/blog/${slug}`}>
+                        <span>
+                          <Image
+                            objectFit="cover"
+                            layout="fill"
+                            alt={title.slice(0, 8)}
+                            src={thumbnail}
+                            className="absolute inset-0 h-full w-full object-cover "
+                          />
+                        </span>
+                      </Link>
+                    </div>
+                    <div className="h-auto py-4 px-2">
+                      <span className="inline-flex w-full items-center justify-between">
+                        <span className="inline-block rounded border border-primary-500 border-gray-700 py-1 px-2 text-xs font-medium text-gray-600 dark:text-gray-400">
+                          {tagsToShow.map((tag) => (
+                            <Tag key={tag} text={tag} />
+                          ))}
+                        </span>
+                        <dl>
+                          <dd className="text-sm font-normal leading-6 text-gray-500 dark:text-gray-400">
+                            <time dateTime={date}>{formatDate(date)}</time>
+                          </dd>
+                        </dl>
+                      </span>
+                      <h2 className="mt-2 mb-2 font-bold md:text-xl">
+                        <Link
+                          href={`/blog/${slug}`}
+                          className="text-gray-800 transition duration-500 ease-in-out hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-500"
+                        >
+                          {title}
+                        </Link>
+                      </h2>
+                      <p className="h-auto text-sm font-normal tracking-wide text-gray-500 dark:text-gray-400">
+                        {summary}
+                      </p>
+                    </div>
+                  </div>
+                </article>
+              )
+            })}
+          </div>
+        </div>
+        {/*
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && 'No posts found.'}
           {posts.slice(0, MAX_DISPLAY).map((post) => {
@@ -28,14 +161,14 @@ export default function Home({ posts }) {
                   <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
                     <dl>
                       <dt className="sr-only">Published on</dt>
-                      <dd className="text-base leading-6 font-medium text-gray-500 dark:text-gray-400">
+                      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
                         <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
                       </dd>
                     </dl>
                     <div className="space-y-5 xl:col-span-3">
                       <div className="space-y-6">
                         <div>
-                          <h2 className="text-2xl leading-8 font-bold tracking-tight">
+                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
                             <Link
                               href={`/blog/${slug}`}
                               className="text-gray-900 dark:text-gray-100"
@@ -53,11 +186,11 @@ export default function Home({ posts }) {
                           {summary}
                         </div>
                       </div>
-                      <div className="text-base leading-6 font-medium">
+                      <div className="text-base font-medium leading-6">
                         <Link
                           href={`/blog/${slug}`}
                           className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                          aria-label={`Read more: "${title}"`}
+                          aria-label={`Read "${title}"`}
                         >
                           Read more &rarr;
                         </Link>
@@ -68,14 +201,15 @@ export default function Home({ posts }) {
               </li>
             )
           })}
-        </ul>
+        </ul> */}
       </div>
+      <hr className="border-gray-200 dark:border-gray-700" />
       {posts.length > MAX_DISPLAY && (
-        <div className="flex justify-end text-base leading-6 font-medium">
+        <div className="mt-5 flex justify-end text-lg font-normal leading-6">
           <Link
             href="/blog"
-            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-            aria-label="All posts"
+            className="mt-2 mr-3 rounded-lg border border-primary-500 py-1 px-3 text-sm font-medium uppercase text-primary-500 transition duration-500 ease-in-out hover:bg-primary-500 hover:text-gray-100 dark:hover:text-gray-900"
+            aria-label="all posts"
           >
             All Posts &rarr;
           </Link>
--- components/Image.tsx
+++ components/Image.tsx
@@ -1,9 +1,47 @@
-import NextImage, { ImageProps } from 'next/image'
+'use client'
 
-const basePath = process.env.BASE_PATH
+import NextImage from 'next/image'
+import React from 'react'
+import cn from 'utils/cn'
 
-const Image = ({ src, ...rest }: ImageProps) => (
-  <NextImage src={`${basePath || ''}${src}`} {...rest} />
-)
+/**
+ * The props of {@link Image}.
+ */
+export type ImageProps = {
+  /**
+   * The image classes to pass to the image.
+   */
+  imageClassName?: string
+  /**
+   * Should the image be lazy loaded?
+   */
+  lazy?: boolean
+} & React.ComponentPropsWithoutRef<typeof NextImage>
 
+const Image = (props: ImageProps) => {
+  const { alt, src, className, imageClassName, lazy = true, ...rest } = props
+  const [isLoading, setLoading] = React.useState(true)
+
+  return (
+    <div
+      className={cn('overflow-hidden', isLoading && 'animate-pulse', className)}
+      data-testid="image-container"
+    >
+      <NextImage
+        className={cn(
+          'transition-[scale,filter] duration-700',
+          isLoading && 'scale-[1.02] blur-xl grayscale',
+          imageClassName
+        )}
+        src={src}
+        alt={alt}
+        loading={lazy ? 'lazy' : undefined}
+        priority={!lazy}
+        quality={100}
+        onLoadingComplete={() => setLoading(false)}
+        {...rest}
+      />
+    </div>
+  )
+}
 export default Image
